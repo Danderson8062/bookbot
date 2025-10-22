8a9cb0ecbbafbc68e3b175295bb41a1ca1ac7bd0
Danderson8062 <dananderson8062@gmail.com>
2025-10-22 13:19:40 -0400

    updated new main file

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 def get_book_text(text):
-    with open(text) as f:
-            file_contents = f.read()
+    with open(text) as w:
+            file_contents = w.read()
             return file_contents
 
 def main():
       file_contents = get_book_text("books/frankenstein.txt")
-      print(file_contents)
+      words=file_contents.split()
+      word_count=len(words)
+      print(f"Found {word_count} total words")
 
 main()
