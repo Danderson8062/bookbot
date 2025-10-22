b0925a9066e532a8776c29dc91718bd8eb13c349
Danderson8062 <dananderson8062@gmail.com>
2025-10-22 12:44:36 -0400

    updated get book text function

--- get_book_text.py
+++
@@ -1,4 +0,0 @@
-def get_book_text(text):
-    with open(workspace/github.com/bootdotdev/bookbot/books/frankenstein.txt) as f:
-            file_contents = f.read()
-    print(file_contents)
--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-def main()
-    
+def get_book_text(text):
+    with open(text) as f:
+            file_contents = f.read()
+            return file_contents
+
+def main():
+      file_contents = get_book_text("books/frankenstein.txt")
+      print(file_contents)
+
+main()
